650d28e061cfc20c7ff60d667a6ee91677770998
Tonny <tonnymakondesa6@gmail.com>
2022-08-06 21:03:54 +0300

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+const q1 = -2;
+const q2 = 12;
+const q3 = 10;
+const q4 = 5;
+const q5 = 5;
+
+const [q1State, q1SetState] = useState(0);
+const [q2State, q2SetState] = useState(0);
+const [q3State, q3SetState] = useState(0);
+const [q4State, q4SetState] = useState(0);
+const [q5State, q5SetState] = useState(0);
+
+function submit(){
+  let comp = [q1, q2, q3, q4, q5];
+  let ans = [q1State, q2State, q3State, q4State, q5State];
+  let calculator = [];
+  let a = 0;
+
+  for(a; a<comp.length; a++){
+    if(comp[a]==ans[a]){
+      calculator.push(a);
+    }
+  }
+
+  let total = (calculator.length/ comp.length) * 100;
+
+  document.getElementById('output').innerHTML = "You got " + total + "%";
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='div1'>
+        <ol>
+          <li>What's 2 - 4?</li>
+          <input type="number" onChange={(event)=>{q1SetState(event.target.value)}}/>
+          <li>What's 3 times 4?</li>
+          <input type="number" onChange={(event)=>{q2SetState(event.target.value)}}/>
+          <li>What's 5 + 5?</li>
+          <input type="number" onChange={(event)=>{q3SetState(event.target.value)}}/>
+          <li>What's 20 / 4 ?</li>
+          <input type="number" onChange={(event)=>{q4SetState(event.target.value)}}/>
+          <li>What's the square root of 25?</li>
+          <input type="number" onChange={(event)=>{q5SetState(event.target.value)}}/>
+        </ol>
+        <button className='button' onClick={()=>{submit()}}>Submit</button>
+      </div>
+      <p id='output'></p>
     </div>
   );
 }
